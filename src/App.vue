3b4522528cdf4b656ef1efdae3edc0fe14ad5a1a
cin-onishi <ohnishi-y@cin-group.com>
2020-07-25 04:17:23 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <p>{{ count }}</p>
+    <p>
+      <input type="button" value="-" v-on:click="minus">
+      {{ count }}
+      <input type="button" value="+" v-on:click="plus">
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  // components: {store},
+
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    minus(){
+      this.$store.commit('minus')
+    },
+    plus(){
+      this.$store.commit('plus')
+    }
+  }
+}
+</script>
