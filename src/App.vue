58e1a0d910e0e565a90c37e9632cfa06ee0c7adc
cin-onishi <ohnishi-y@cin-group.com>
2020-07-29 18:08:32 +0900

    fix:vuex

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,86 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <p>{{ count }}</p>
     <p>
       <input type="button" value="-" v-on:click="minus">
       {{ count }}
       <input type="button" value="+" v-on:click="plus">
     </p>
+  </div> -->
+
+  <div>
+    <form v-on:submit.prevent="onclick">
+      <label for="isbn">ISBN:</label>
+      <input type="text" id="isbn" v-model="isbn">
+      <label for="title">書名：</label>
+      <input type="text" id="title" v-model="title">
+    <label for="price">価格：</label>
+    <input type="number" id="price" v-model="price" /><br />
+    <input type="submit" value="登録" />
+    </form>
+    <hr>
+    <p>書籍は全部で{{booksCount}}冊あります。</p>
+    <ul v-for="b of getBooksByPrice(2500)" v-bind:key="b.isbn">
+      <li>{{b.title}}（{{b.price}}円）<br />ISBN：{{b.isbn}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import { mapActions } from 'vuex'
+
 export default {
   name: 'app',
   // components: {store},
 
-  computed: {
-    count () {
-      return this.$store.state.count
+  computed: mapGetters(['booksCount', 'getBooksByPrice']),
+
+  // フォーム内で扱う情報を準備
+  data() {
+    return {
+      isbn: '',
+      title: '',
+      price: 0
     }
   },
   methods: {
-    minus(){
-      this.$store.commit('minus')
-    },
-    plus(){
-      this.$store.commit('plus')
+    ...mapActions({add: 'addAsync'}),
+
+    // 登録ボタンクリックでストアに反映
+    onclick() {
+      this.add({
+        book: {
+          isbn: this.isbn, title: this.title, price: this.price
+        }
+      })
     }
   }
+
+
+  // computed: {
+  //   count () {
+  //     return this.$store.state.count
+  //   }
+  // },
+  // methods: {
+  //   minus(){
+  //     this.$store.commit('minus')
+  //   },
+  //   plus(){
+  //     this.$store.commit('plus')
+  //   }
+  // }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,81 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { ADD_BOOK } from '../mutation-types'
+// import { filter } from 'core-js/fn/array'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   // strict: true,
   state: {
-    count: 0
+    books: [
+      {
+        isbn: '978-4-8222-5389-9',
+        title: '作って楽しむプログラミング HTML5超入門',
+        price: 2000
+      },
+      {
+        isbn: '978-4-7981-5757-3',
+        title: 'JavaScript逆引きレシピ',
+        price: 2800
+      },
+      {
+        isbn: '978-4-7741-9763-0',
+        title: '3ステップでしっかり学ぶ Python 入門',
+        price: 2480
+      },
+      {
+        isbn: '978-4-7981-5382-7',
+        title: '独習C# 新版',
+        price: 3600
+      },
+      {
+        isbn: '978-4-8399-6644-7',
+        title: 'たのしいラズパイ電子工作ブック',
+        price: 1900
+      },
+    ]
+  },
+
+  getters: {
+    booksCount(state){
+      return state.books.length
+    },
+    getBooksByPrice(state){
+      return price => {
+        // filter はjavascriptの標準メゾット
+        // 配列に対してfilterメソッドを指定
+        // array.filter(callback [,that]);
+        // state.booksの中からprice(state)以上の本を除外する関数
+        return state.books.filter(book => book.price < price)
+      }
+    }
   },
   mutations: {
-    minus(state) {
-      state.count--
-    },
-    plus(state) {
-      state.count++
+    // ミューテーションで引数を持つことも可能
+    // この引数をpayload(ペイロード)を呼ぶ
+    addBook(state, payload){
+      // payload.bookで本の情報を登録
+      // 登録する情報のフォームはApp.vue内で定義
+      // bookプロパティーに書籍情報を渡す
+      state.books.push(payload.book)
+    }
+  },
+
+  // 非同期処理をする場合はアクションとして切り出す
+  // 関連するミューテーションを呼び出してアクションで更新をする
+  actions: {
+    addAsync(context, payload){
+      console.log('context');
+      console.log(context);
+      console.log('payload');
+      console.log(payload);
+      // context booksオブジェクトを取得
+      // payload.bookで本の情報を登録
+      // bookプロパティーに書籍情報を渡す
+      setTimeout(function(){
+        context.commit(ADD_BOOK, payload)
+      }, 5000)
     }
   }
 })
